eb87aa6c17e9dc2a3ec07efa12b47f7627872f47
Johan Garcia <johangarcia@outlook.com>
2023-07-04 15:33:20 +0000

    finish example liskov principle

---
+++ liskov_substitution/good.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self,width, height) -> None:
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.width * self.height
+    
+class Scuare(Shape):
+    def __init__(self, side):
+        self.side = side
+    
+    def calculate_area(self):
+        return self.side ** 2
